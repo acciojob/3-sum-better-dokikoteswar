59db58fbbaa517567501b435dae55d184a6316d7
dokikoteswar <139772409+dokikoteswar@users.noreply.github.com>
2023-09-26 14:42:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,33 @@
 function threeSum(arr, target) {
-// write your code here
-  
+// wri
+
+	  S.sort((a, b) => a - b);
+
+    // Initialize closestSum to a large value.
+    let closestSum = Infinity;
+
+    for (let i = 0; i < S.length - 2; i++) {
+        let left = i + 1;
+        let right = S.length - 1;
+
+        while (left < right) {
+            const currentSum = S[i] + S[left] + S[right];
+
+            if (Math.abs(currentSum - target) < Math.abs(closestSum - target)) {
+                closestSum = currentSum;
+            }
+
+            if (currentSum < target) {
+                left++;
+            } else if (currentSum > target) {
+                right--;
+            } else {
+                return currentSum; // Exact match found.
+            }
+        }
+    }
+
+    return closestSum;
 }
 
 module.exports = threeSum;
